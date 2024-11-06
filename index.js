942e5b027d1432ac660736e322db0c4b52cbcd53
LumaSantos <63914574+LumaSantos@users.noreply.github.com>
2024-11-06 20:41:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+class novaPessoa {
+    constructor(nome, anoNascimento, cidadeNascimento) {
+    this.nome = nome;
+    this.anoNascimento = anoNascimento;
+    this.cidadeNascimento = cidadeNascimento;
+}
+apresentar() {
+    var idade = 2024 - this.anoNascimento;
+    console.log("O " + this.nome + " é de " + this.cidadeNascimento + " e tem " + idade + " anos")
+}
+    }
+
+    let nome = prompt("Digite o seu nome");
+    let anoNascimento = parseInt(prompt("Digite o seu ano de nascimento:"));
+    let cidadeNascimento = prompt("Digite a sua idade de nascimento:"); 
+   
+    let pessoa = new novaPessoa(nome, anoNascimento, cidadeNascimento);
+
+   pessoa.apresentar()
